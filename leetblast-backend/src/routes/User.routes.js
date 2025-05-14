aac7d32df55346b66446abd99c0ca5305439c269
Nilesh Kurrey <nileshkurrey1@gmail.com>
2025-05-14 17:56:25 +0530

    Add middleware and validator to check user information is correct

--- leetblast-backend/src/routes/User.routes.js
+++ leetblast-backend/src/routes/User.routes.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import { userRegistrationValidator } from '../validator'
+import { validate } from '../middlewares/validator.middlewre'
 
 const authRoutes = express.Router()
 
-authRoutes.route('/register')
+authRoutes.route('/register').post(userRegistrationValidator(),validate)
 authRoutes.route('/login')
